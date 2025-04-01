bc06258add47ae66e5f3220ad81e8cafb3426997
ZeroDark <Chaitanyajain.official@gmail.com>
2025-04-01 10:55:02 +0530

    updated manifest.json edited the main.ts and styles.css

--- main.ts
+++ main.ts
@@ -1,85 +1,59 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface DarkLightSettings {
+	selectedTheme: string;
+	customThemes: string[];
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: DarkLightSettings = {
+	selectedTheme: 'default',
+	customThemes: []
+};
 
-export default class ObsiStylePlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class DarklightPlugin extends Plugin {
+	settings: DarkLightSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'ObsiStyle', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This plugin is enabled');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		// Add ribbon icon to open settings directly
+		const ribbonIconEl = this.addRibbonIcon("palette", "DarkLight Settings", async () => {
+			// Open the settings modal
+			this.app.workspace.trigger("open-settings");
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+			// Wait a bit for settings to open, then navigate to your plugin's settings tab
+			setTimeout(() => {
+				const settingsContainer = document.querySelector(".modal-container");
+				if (settingsContainer) {
+					const pluginTab = settingsContainer.querySelector(`[data-id="${this.manifest.id}"]`);
+					if (pluginTab) {
+						(pluginTab as HTMLElement).click();
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
 				}
-			}
+			}, 200); // Adjust delay if needed
+
+			setTimeout(() => {
+				const settingsTab = document.querySelector(`.setting-item[data-id="${this.manifest.id}"]`) as HTMLElement;
+				if (settingsTab) {
+					settingsTab.click(); // Simulate a click to open Darklight settings
+				}
+			}, 500); // Wait for settings to open
+			new Notice("Opening Darklight settings...");
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		ribbonIconEl.addClass("darklight-ribbon-icon");
+
+		// Apply the selected theme
+		this.applyTheme(this.settings.selectedTheme);
+
+		// Add settings tab
+		this.addSettingTab(new DarklightSettingTab(this.app, this));
 	}
 
 	onunload() {
-
+		console.log("Darklight Plugin Unloaded");
 	}
 
 	async loadSettings() {
@@ -89,46 +63,56 @@ export default class ObsiStylePlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
-}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+	applyTheme(theme: string) {
+		const styleEl = document.getElementById("darklight-theme-style");
+		if (styleEl) styleEl.remove();
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+		const newStyleEl = document.createElement("style");
+		newStyleEl.id = "darklight-theme-style";
+		newStyleEl.textContent = `@import url('styles.css');`;
+		document.head.appendChild(newStyleEl);
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: ObsiStylePlugin;
+class DarklightSettingTab extends PluginSettingTab {
+	plugin: DarklightPlugin;
 
-	constructor(app: App, plugin: ObsiStylePlugin) {
+	constructor(app: App, plugin: DarklightPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
-
+		const { containerEl } = this;
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+			.setName("Select Theme")
+			.setDesc("Choose a pre-installed or custom theme")
+			.addDropdown(dropdown => {
+				dropdown.addOption("default", "Default Theme");
+				this.plugin.settings.customThemes.forEach(theme => dropdown.addOption(theme, theme));
+				dropdown.setValue(this.plugin.settings.selectedTheme);
+				dropdown.onChange(async (value) => {
+					this.plugin.settings.selectedTheme = value;
 					await this.plugin.saveSettings();
-				}));
+					this.plugin.applyTheme(value);
+				});
+			});
+
+		new Setting(containerEl)
+			.setName("Add Custom Theme")
+			.setDesc("Enter the name of your custom theme")
+			.addText(text => {
+				text.setPlaceholder("Theme Name");
+				text.onChange(async (value) => {
+					if (value && !this.plugin.settings.customThemes.includes(value)) {
+						this.plugin.settings.customThemes.push(value);
+						await this.plugin.saveSettings();
+						this.display(); 
+					}
+				});
+			});
 	}
 }
